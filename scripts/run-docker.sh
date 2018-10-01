85a6dd9cfad5be35fb85fb3b8c36f7962dd621dd
jottr <j@ovvvo.co>
2018-10-01 20:11:15 +0200

    Fix quoting.

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -7,11 +7,11 @@ if [ "$UNAME" = Darwin ]; then
 	# Workaround for mac readlink not supporting -f.
 	REPOROOT=$PWD
 else
-	REPOROOT="$(dirname $(readlink -f $0))/../"
+	REPOROOT="$(dirname "$(readlink -f "$0")")/../"
 fi
 
 IMAGE_NAME=termux/package-builder
-: ${CONTAINER_NAME:=termux-package-builder}
+: "${CONTAINER_NAME:=termux-package-builder}"
 
 USER=builder
 
@@ -23,17 +23,17 @@ docker start $CONTAINER_NAME > /dev/null 2> /dev/null || {
 	       --detach \
 	       --env HOME=$HOME \
 	       --name $CONTAINER_NAME \
-	       --volume $REPOROOT:$HOME/termux-packages \
+	       --volume "$REPOROOT":$HOME/termux-packages \
 	       --tty \
 	       $IMAGE_NAME
     if [ "$UNAME" != Darwin ]; then
-        if [ $(id -u) -ne 1000 -a $(id -u) -ne 0 ]
+        if [ "$(id -u)" -ne 1000 ] && [ "$(id -u)" -ne 0 ]
         then
             echo "Changed builder uid/gid... (this may take a while)"
-            docker exec --tty $CONTAINER_NAME chown -R $(id -u) $HOME
-            docker exec --tty $CONTAINER_NAME chown -R $(id -u) /data
-            docker exec --tty $CONTAINER_NAME usermod -u $(id -u) builder
-            docker exec --tty $CONTAINER_NAME groupmod -g $(id -g) builder
+            docker exec --tty $CONTAINER_NAME chown -R "$(id -u)" "${HOME}"
+            docker exec --tty $CONTAINER_NAME chown -R "$(id -u)" /data
+            docker exec --tty $CONTAINER_NAME usermod -u "$(id -u)" builder
+            docker exec --tty $CONTAINER_NAME groupmod -g "$(id -g)" builder
         fi
     fi
 }
@@ -41,7 +41,7 @@ docker start $CONTAINER_NAME > /dev/null 2> /dev/null || {
 if [ "$#" -eq  "0" ]; then
 	docker exec --interactive --tty --user $USER $CONTAINER_NAME bash
 else
-	docker exec --interactive --tty --user $USER $CONTAINER_NAME $@
+	docker exec --interactive --tty --user $USER $CONTAINER_NAME "${@}"
 fi
 
 
